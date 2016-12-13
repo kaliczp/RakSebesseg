ed5d006199a08aec36eb9985fa22313711651f1c
Péter Kalicz <kaliczp@gmail.com>
2016-12-13 14:03:29 +0100

    make.ksz function
    Az első keresztszelvényt kiszedő példa függvény. Az adatok ábrázolása

--- Beolvaso.R
+++ Beolvaso.R
@@ -1,2 +1,18 @@
 vs <- read.csv2("vizseb2014.csv", dec=".", stringsAsFactors = FALSE, head=F)
-vs[7:12,]
+## Egy keresztszelvény
+test <- vs[6:12,]
+
+## Keresztszelvény létrehozása
+make.ksz <- function(x){
+  ## x: Az adott keresztszelvény mérés első két sora
+  fuggely.index <- is.na(x[1,])
+  ## Függély helyének keresése
+  fuggely.hely <- x[1, !fuggely.index]
+  fuggely.hely <- as.numeric(fuggely.hely[-(1:2)])
+  ## Mélység
+  melyseg <- as.numeric(x[2,fuggely.index])
+  ## Adattábla összeállítás
+  data.frame(fuggely.hely,-melyseg)
+}
+
+plot(make.ksz(test), typ="l")
